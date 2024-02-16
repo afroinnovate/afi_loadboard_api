40c59f2efd36ccee6ac83f07bea0eb73a04c7b8e
kor.tot <kor.tot@education.ne.gov>
2024-02-16 11:58:19 -0600

    Notification module

---
+++ Frieght.Api/Infrastructure/Notifications/IMessageSender.cs
@@ -0,0 +1,17 @@
+﻿namespace Frieght.Api.Infrastructure.Notifications;
+
+/// <summary>
+/// Interface for sending emails.
+/// Sends an  email asynchronously.
+/// </summary>
+/// <param name="toEmail">Recipient's email address.</param>
+/// <param name="subject">Email subject.</param>
+/// <param name="Body">body content of the email.</param>
+/// <returns>A task representing the asynchronous operation.</returns>
+/// </summary>
+
+public interface IMessageSender
+{
+    Task SendEmailAsync(string recipient, string subject, string body);
+    Task SendSmsAsync(string toPhoneNumber, string messageBody);
+}
