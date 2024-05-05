3188af93755976f61ffb6bb547ed6873060add58
tew9 <tango.tew1@accesscfa.com>
2024-05-05 12:26:38 -0500

    Fix role issue with the shipper

--- Auth.Min.API/Models/Roles.cs
+++ Auth.Min.API/Models/Roles.cs
@@ -5,6 +5,7 @@ namespace Auth.Min.API;
     public string? Admin { get; set; }
     public string? Support { get; set; }
     public string? Carrier { get; set; }
+    public string? Shipper { get; set; }
     public string? GovShipper { get; set; }
     public string? CorporateShipper { get; set; }
     public string? IndependentShipper { get; set; }
